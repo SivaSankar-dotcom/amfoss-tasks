8d6e1d3035f2d8387eb3c8f63b7592b1a604a179
Siva Sankar <rameshsankarsiva@gmail.com>
2024-12-30 12:44:16 +0530

    Add files via upload

---
+++ Task03/index.js
@@ -0,0 +1,41 @@
+var wd = new Audio('sounds/crash.mp3')
+ function wdrum()
+ {
+    wd.play();
+ }
+
+ var ad = new Audio('sounds/kick-bass.mp3')
+ function adrum()
+ {
+    ad.play()
+ }
+
+ var sd = new Audio('sounds/snare.mp3')
+ function sdrum()
+ {
+    sd.play()
+ }
+
+ var dd = new Audio('sounds/tom-1.mp3')
+ function ddrum()
+ {
+    dd.play()
+ }
+
+ var jd = new Audio('sounds/tom-2.mp3')
+ function jdrum()
+ {
+    jd.play()
+ }
+
+ var kd = new Audio('sounds/tom-2.mp3')
+ function kdrum()
+ {
+    kd.play()
+ }
+
+ var ld = new Audio('sounds/tom-3.mp3')
+ function ldrum()
+ {
+    ld.play()
+ }
